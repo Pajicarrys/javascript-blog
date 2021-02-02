422458bc7b94259f30fa07f34e0249e5d09b877c
Tomasz Pająk <pajaktomasz@hotmail.com>
2021-02-02 22:35:17 +0100

    Add_active_class_to_clicked_link

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const titleClickHandler = function(event){
+    
+    const clickedElement = this;
+    
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
   
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -11,9 +14,15 @@ const titleClickHandler = function(event){
     }
 
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+
+
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post');
 
