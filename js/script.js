cfe71f688c76fd2d2ae226c465e2d32c61accbff
Tomasz Pająk <pajaktomasz@hotmail.com>
2021-02-03 21:03:48 +0100

    Change_Articles_By_Click_Working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const titleClickHandler = function(event){
     
+    event.preventDefault();
     const clickedElement = this;
-    
-    console.log('Link was clicked!');
-    console.log(event);
+
+
+    console.log('Event '+ event);
   
     /* [DONE] remove class 'active' from all article links  */
   
@@ -14,7 +15,7 @@ const titleClickHandler = function(event){
     }
 
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
 
     console.log('clickedElement:', clickedElement);
@@ -30,11 +31,26 @@ const titleClickHandler = function(event){
      activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log('aticleSelector '+ articleSelector);
+
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /*  [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
+
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+       
+    //targetArticle.classList.add('active');
+       
+
+
   }
   
   const links = document.querySelectorAll('.titles a');
